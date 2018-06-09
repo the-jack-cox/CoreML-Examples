8f5cb52a2f5f4096944f97851875e97594b9526b
Jack Cox <jcox@captechconsulting.com>
2018-06-09 08:32:53 -0400

    Initial Commit

---
+++ MLCore Examples/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MLCore Examples
+//
+//  Created by Jack Cox on 6/9/18.
+//  Copyright © 2018 CapTech Consulting. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
